b8d3f098f5105037809d149afdfaa9af9f09e660
qinxiuchen <qinxiuchen@gmail.com>
2017-11-29 16:47:47 +0800

    filter totalCount = 1

--- line.go
+++ line.go
@@ -54,8 +54,11 @@ func (l *NormalLine) Compute(b *Board) (int, []int, [][][]int, error) {
 				break
 			}
 		}
+		if totalCount == 1 {
+			return reward, lines, lineItemsPos, nil
+		}
 		obtainConfig := config.ObtainsConfig[firstSymbol]
-		if obtainConfig.Reward[totalCount-1] != 0 {
+		if obtainConfig.Reward[totalCount-2] != 0 {
 			reward += obtainConfig.Reward[totalCount-1]
 			lines = append(lines, lineIndex)
 			// lineItemsIndex = append(lineItemsIndex, itemsPos)
