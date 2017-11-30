fc12646bc51ba88bc469e512441b5614d72b6ded
qinxiuchen <qinxiuchen@gmail.com>
2017-11-30 12:39:38 +0800

    fix runner get symbol bug

--- runner.go
+++ runner.go
@@ -28,10 +28,10 @@ func (r *Runner) GetRunnerPos() int {
 //GetSymbol TODO
 func (r *Runner) GetSymbol(which int) int {
 	gearConfig := config.GearsConfig[r.BindGear]
-	index := (r.NowPos + which + 1) / len(gearConfig.Symbols)
-	if index == 0 {
-		index = len(gearConfig.Symbols)
-	}
-	index = index - 1
+	index := (r.NowPos + which) % len(gearConfig.Symbols)
+	// if index == 0 {
+	// 	index = len(gearConfig.Symbols)
+	// }
+	// index = index - 1
 	return gearConfig.Symbols[index]
 }
