baa26217e6113cd17c5f450af7d069375a933544
topgame <topgame@topgamedeMBP-2.lan>
2018-01-31 12:00:28 +0800

    add history move

--- detail/common/his_action.go
+++ detail/common/his_action.go
@@ -42,11 +42,13 @@ func InitMysqlHisModels(dsn string, syncDb bool) error {
 
 //dividingTime 之前的记录会被移动到历史库里,执行时刻，如果不设则直接移动，设置格式 上午6点就设为6 [0-23]
 func MoveOldDataToHis(dividingTime time.Time,execHour int) error {
+
+	logger.Infof("=========================")
 	tn := time.Now().Local()
 	if execHour != -1 && tn.Hour() != execHour {
 		return nil
 	}
-
+	logger.Infof("=========================")
 	container := []*Round{}
 	n,err := O.QueryTable(&Round{}).Filter("CreatedAt__lt",dividingTime).All(&container)
 
@@ -54,7 +56,7 @@ func MoveOldDataToHis(dividingTime time.Time,execHour int) error {
 		logger.Error(err.Error())
 		return err
 	}
-
+	logger.Infof("=========================")
 	logger.Infof(n)
 	for i := 0; i < int(n); i++ {
 		r := container[i]
